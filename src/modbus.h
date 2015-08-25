925b7cd9044a8c5f62758829ac9b652dbc367161
cuki <mauriciocukier@gmail.com>
2015-08-24 23:37:21 -0300

    biblioteca estatica ok

--- src/modbus.h
+++ src/modbus.h
@@ -17,11 +17,13 @@
 #define write_multiple_coils		0x0F
 #define Write_multiple_registers	0x10
 
-#define high	1
-#define low		0
+#define high						1
+#define low							0
+#define request_size				8
 
-long CRC16(const int *nData, long wLength);
-int toInt(long in, short side);
-int *make_read_request(int dev_addr, long from, long to);
+unsigned short CRC16(unsigned char *nData, unsigned short wLength);
+unsigned char *make_read_request(unsigned char dev_addr, unsigned short from,
+		unsigned short to);
+unsigned char send_request(unsigned char *output);
 
 #endif /* MODBUS_H_ */
