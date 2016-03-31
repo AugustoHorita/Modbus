35d4646d891ef47217b5e8acdf6ea046c07ca8ab
cuki2 <mauricio@krebs.com.br>
2016-03-31 09:54:52 -0300

    V1.0

--- src/modbus.h
+++ src/modbus.h
@@ -8,26 +8,34 @@
 #ifndef MODBUS_H_
 #define MODBUS_H_
 
-#define read_coils					0x01
-#define read_discrete_inputs		0x02
-#define read_holding_registers		0x03
-#define read_input_registers		0x04
-#define write_single_coil			0x05
-#define write_single_register		0x06
-#define write_multiple_coils		0x0F
-#define Write_multiple_registers	0x10
+#define READ_COILS					0x01
+#define READ_DISCRETE_INPUTS		0x02
+#define READ_HOLDING_REGISTERS		0x03
+#define READ_INPUT_REGISTERS		0x04
+#define WRITE_SINGLE_COIL			0x05
+#define WRITE_SINGLE_REGISTER		0x06
+#define WRITE_MULTIPLE_COILS		0x0F
+#define WRITE_MULTIPLE_REGISTERS	0x10
 
-#define high						1
-#define low							0
-#define request_size				8
-#define buffer_s					256
+#define HIGH						1
+#define LOW							0
+#define REQUEST_SIZE				8
+#define BUFFER_S					256
 
+extern const unsigned short wCRCTable[];
+extern unsigned char makeByte(unsigned short word, unsigned char index);
+extern unsigned short makeWord(unsigned char hiByte, unsigned char loByte);
+extern unsigned short troca(unsigned short in);
 extern unsigned short CRC16(unsigned char *nData, unsigned short wLength);
 extern unsigned char *make_request(unsigned char dev_addr, unsigned short from,
 		unsigned short to, unsigned char type);
 extern unsigned char *make_read_request(unsigned char dev_addr,
 		unsigned short from, unsigned short nr);
 extern unsigned char *make_write_request(unsigned char dev_addr,
+		unsigned short reg_addr, unsigned short reg_value);
+extern unsigned char *make_discrete_inputs_request(unsigned char dev_addr,
+		unsigned short reg_addr, unsigned short reg_value);
+extern unsigned char *make_read_coils_request(unsigned char dev_addr,
 		unsigned short reg_addr, unsigned short reg_value);
 extern int get_device(int fd, unsigned char dev_addr, unsigned short reg_addr,
 		unsigned short reg_data, unsigned char *device_memorie);
